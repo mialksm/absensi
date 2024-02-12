eaa270df79e017baf417e78ad2e7f9a47ae94101
Zen <fauz.fauz23@gmail.com>
2024-02-12 14:26:48 +0700

    add

--- sw-mod/location.php
+++ sw-mod/location.php
@@ -1,4 +1,8 @@
 <?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
 
 if (empty($connection)) {
     header('location:./404');
@@ -6,8 +10,19 @@ if (empty($connection)) {
     $query_isp   = "SELECT * FROM isp limit 1";
     $result_isp  = mysqli_query($connection, $query_isp);
     $myip = $_SERVER['REMOTE_ADDR'];
+
+    $client = new Client();
+    $response = $client->request('GET', "http://ip-api.com/json/" . $myip);
+
+    // Get the response body as a string
+    $body = $response->getBody();
+
     while ($row = mysqli_fetch_assoc($result_isp)) {
         echo ($row['isp_name']);
         echo " (" . $myip . ")";
+        echo " (" . $body['country'] . ")";
+        echo " (" . $body['countryCode'] . ")";
+        echo " (" . $body['city'] . ")";
+        echo " (" . $body['isp'] . ")";
     }
 }
